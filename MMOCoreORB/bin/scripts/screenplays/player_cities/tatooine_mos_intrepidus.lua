8dbee7b489354d38795937bd0d80064eb05adc71
ProjectCrimson <bill.vanderende@gmail.com>
2019-09-24 21:54:43 -0400

    Added an Armorweaver trainer to Mos Intrepidus

--- MMOCoreORB/bin/scripts/screenplays/player_cities/tatooine_mos_intrepidus.lua
+++ MMOCoreORB/bin/scripts/screenplays/player_cities/tatooine_mos_intrepidus.lua
@@ -1,3 +1,5 @@
+-- 2019-02-24 Perkins - added Armorweaver Trainer to Mos Intrepidus,
+--	and split mobile and scene objects
 TatooineMosIntrepidusScreenPlay = ScreenPlay:new {
 	numberOfActs = 1,
 	screenplayName = "TatooineMosIntrepidusScreenPlay"
@@ -8,13 +10,19 @@ registerScreenPlay("TatooineMosIntrepidusScreenPlay", true)
 function TatooineMosIntrepidusScreenPlay:start()
 	if (isZoneEnabled("tatooine")) then
 		self:spawnMobiles()
+		self:spawnSceneObjects()
 	end
 end
 
+function TatooineMosIntrepidusScreenPlay:spawnSceneObjects()	
+	spawnSceneObject("tatooine", "object/tangible/terminal/terminal_character_builder.iff", -3090, 0, -5700, 0, math.rad(90))
+end
+
 function TatooineMosIntrepidusScreenPlay:spawnMobiles()	
+	spawnMobile("tatooine", "trainer_armorweaver",1,-3013,0,-5690,270,0)
+
 	pNpc = spawnMobile("tatooine", "imperial_recruiter",0,-3105,0,-5692,90,0)
 	self:setMoodString(pNpc, "npc_imperial")
 	
 	spawnMobile("tatooine", "rebel_recruiter", 0, -3105, 0, -5707, 90, 0)	
-	spawnSceneObject("tatooine", "object/tangible/terminal/terminal_character_builder.iff", -3090, 0, -5700, 0, math.rad(90))
 end
