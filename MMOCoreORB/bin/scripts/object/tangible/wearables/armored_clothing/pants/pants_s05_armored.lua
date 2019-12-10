6a8b90bdad6b58b75d55b0c53f713eb4744cabd5
user <user@gmail.com>
2019-12-09 22:49:42 -0500

    Updates to armor, armorweave, and segments
    Lowered "missed" values in armor balance pass for both segments and armor appearance

--- MMOCoreORB/bin/scripts/object/tangible/wearables/armored_clothing/pants/pants_s05_armored.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/armored_clothing/pants/pants_s05_armored.lua
@@ -73,7 +73,7 @@ object_tangible_wearables_armored_clothing_pants_pants_s05_armored = object_tang
   experimentalGroupTitles = {"null", "null", "null", "exp_durability", "exp_quality", "exp_resistance", "null", "null", "null", "null", "null", "exp_resistance", "null"},
   experimentalSubGroupTitles = {"null", "null", "sockets", "hit_points", "armor_effectiveness", "armor_integrity", "armor_health_encumbrance", "armor_action_encumbrance", "armor_mind_encumbrance", "armor_rating", "armor_special_type", "armor_special_effectiveness", "armor_special_integrity"},
   experimentalMin = {0, 0, 0, 1000, 1, 30000, 0, 0, 0, 1, 0, 0, 0},
-  experimentalMax = {0, 0, 0, 1000, 30, 50000, 0, 0, 0, 1, 0, 0, 0},
+  experimentalMax = {0, 0, 0, 1000, 17, 50000, 0, 0, 0, 1, 0, 0, 0},
   experimentalPrecision = {0, 0, 0, 0, 10, 0, 0, 0, 0, 0, 0, 0, 0},
   experimentalCombineType = {0, 0, 4, 1, 1, 1, 1, 1, 1, 4, 4, 4, 1},
 }
