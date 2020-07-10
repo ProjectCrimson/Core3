1d6d151150357a580a49cc5648909e8b986dcef3
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-07-10 09:32:34 -0500

    Update basilisk_war_droid.lua

--- MMOCoreORB/bin/scripts/object/mobile/vehicle/basilisk_war_droid.lua
+++ MMOCoreORB/bin/scripts/object/mobile/vehicle/basilisk_war_droid.lua
@@ -1,7 +1,7 @@
 
 object_mobile_vehicle_basilisk_war_droid = object_mobile_vehicle_shared_basilisk_war_droid:new {
 	templateType = VEHICLE,
-	decayRate = 15, -- Damage tick per decay cycle
+	decayRate = 5, -- Damage tick per decay cycle --Starkiller - lowered from 15 to 5.
 	decayCycle = 600 -- Time in seconds per cycle
 }
 ObjectTemplates:addTemplate(object_mobile_vehicle_basilisk_war_droid, "object/mobile/vehicle/basilisk_war_droid.iff")
