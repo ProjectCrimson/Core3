d8d0334150dfe8dec0329203418d1370b5cd22ae
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-07-10 09:39:09 -0500

    Update tcg_military_transport.lua

--- MMOCoreORB/bin/scripts/object/mobile/vehicle/tcg_military_transport.lua
+++ MMOCoreORB/bin/scripts/object/mobile/vehicle/tcg_military_transport.lua
@@ -1,7 +1,7 @@
 
 object_mobile_vehicle_tcg_military_transport = object_mobile_vehicle_shared_tcg_military_transport:new {
 	templateType = VEHICLE,
-	decayRate = 15, -- Damage tick per decay cycle
+	decayRate = 10, -- Damage tick per decay cycle --Starkiller - Updated from 15 to 10.
 	decayCycle = 600 -- Time in seconds per cycle
 }
 ObjectTemplates:addTemplate(object_mobile_vehicle_tcg_military_transport, "object/mobile/vehicle/tcg_military_transport.iff")
