ea5d05168a1e5c57c84dfb9147742bbcecbe4514
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-07-10 09:36:00 -0500

    Update landspeeder_xp38.lua

--- MMOCoreORB/bin/scripts/object/mobile/vehicle/landspeeder_xp38.lua
+++ MMOCoreORB/bin/scripts/object/mobile/vehicle/landspeeder_xp38.lua
@@ -43,7 +43,7 @@
 
 object_mobile_vehicle_landspeeder_xp38 = object_mobile_vehicle_shared_landspeeder_xp38:new {
 	templateType = VEHICLE,
-	decayRate = 15, -- Damage tick per decay cycle
+	decayRate = 10, -- Damage tick per decay cycle --Starkiller - Updated from 15 to 10.
 	decayCycle = 600 -- Time in seconds per cycle
 }
 
