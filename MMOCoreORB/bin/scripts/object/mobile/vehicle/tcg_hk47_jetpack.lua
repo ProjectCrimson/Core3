ad1a1484e5e193348add51e7aea60a275d6ad84b
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-07-10 09:38:15 -0500

    Update tcg_hk47_jetpack.lua

--- MMOCoreORB/bin/scripts/object/mobile/vehicle/tcg_hk47_jetpack.lua
+++ MMOCoreORB/bin/scripts/object/mobile/vehicle/tcg_hk47_jetpack.lua
@@ -2,7 +2,7 @@
 object_mobile_vehicle_tcg_hk47_jetpack = object_mobile_vehicle_shared_tcg_hk47_jetpack:new {
 	templateType = VEHICLE,
 		-- woohoori 12042019 decay rate moved to Tier 3
-	decayRate = 5, -- Damage tick per decay cycle
+	decayRate = 10, -- Damage tick per decay cycle --Starkiller - Updated from 5 to 10.
 	decayCycle = 600 -- Time in seconds per cycle
 }
 ObjectTemplates:addTemplate(object_mobile_vehicle_tcg_hk47_jetpack, "object/mobile/vehicle/tcg_hk47_jetpack.iff")
