279fc57805e3665f3752931b2f2757349def7d16
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-07-24 15:01:54 -0500

    Update robe_jedi_light_s04.lua

--- MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_light_s04.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_light_s04.lua
@@ -65,13 +65,13 @@ object_tangible_wearables_robe_robe_jedi_light_s04 = object_tangible_wearables_r
 				
 				skillMods = {
 				    {"jedi_force_power_max", 350},
-            {"jedi_force_power_regen", 35},
-            {"force_power_light", 15},
-            {"force_control_light", 15},
-            {"force_manipulation_light", 15}
-            {"lightsaber_toughness", 5},
-            {"forceintimidate_accuracy", 10}
-        },
+        			    {"jedi_force_power_regen", 35},
+       			            {"force_power_light", 15},
+       			            {"force_control_light", 15},
+     			            {"force_manipulation_light", 15}
+      			            {"lightsaber_toughness", 5},
+       			            {"forceintimidate_accuracy", 10}
+   			        },
 
 				noTrade = 1,
 
