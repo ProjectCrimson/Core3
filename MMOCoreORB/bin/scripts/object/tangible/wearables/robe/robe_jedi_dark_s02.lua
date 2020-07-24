0d170972c8c532d98eae5f9ad7ce0c54399c8c3d
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-07-24 16:43:18 -0500

    Update robe_jedi_dark_s02.lua

--- MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_dark_s02.lua
+++ MMOCoreORB/bin/scripts/object/tangible/wearables/robe/robe_jedi_dark_s02.lua
@@ -64,11 +64,11 @@ object_tangible_wearables_robe_robe_jedi_dark_s02 = object_tangible_wearables_ro
 				"object/creature/player/zabrak_female.iff" },
 				
 				skillMods = {
-            {"jedi_force_power_max", 325},
-            {"jedi_force_power_regen", 25},
-            {"force_power_dark", 5},
-            {"force_control_dark", 5},
-            {"force_manipulation_dark", 5}
+           				 {"jedi_force_power_max", 325},
+         			         {"jedi_force_power_regen", 25},
+           				 {"force_power_dark", 5},
+           				 {"force_control_dark", 5},
+           				 {"force_manipulation_dark", 5}
 				},
 
 				noTrade = 1,
