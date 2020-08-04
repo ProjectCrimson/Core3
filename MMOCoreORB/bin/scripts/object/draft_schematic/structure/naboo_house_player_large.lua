9f6f6cbc60413147eebe281ae433beec92a43eae
Sean Scofield <scscofield@hotmail.com>
2020-08-04 11:45:46 -0400

    Wall gods fought back but have been defeated

--- MMOCoreORB/bin/scripts/object/draft_schematic/structure/naboo_house_player_large.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/structure/naboo_house_player_large.lua
@@ -66,7 +66,8 @@ object_draft_schematic_structure_naboo_house_player_large = object_draft_schemat
 	ingredientTemplateNames = {"craft_structure_ingredients_n", "craft_structure_ingredients_n", "craft_structure_ingredients_n", "craft_structure_ingredients_n", "craft_structure_ingredients_n", "craft_structure_ingredients_n"},
 	ingredientTitleNames = {"load_bearing_structure_and_shell", "insulation_and_covering", "foundation", "wall_sections", "power_supply_unit", "storage_space"},
 	ingredientSlotType = {0, 0, 0, 2, 1, 1},
-	resourceTypes = {375, 625, 100, 10, 1, 2},
+	resourceTypes = {"metal", "ore", "ore", "object/tangible/component/structure/shared_wall_module.iff", "object/tangible/component/structure/shared_power_core_unit.iff", "object/tangible/component/structure/shared_structure_storage_section.iff"},
+	resourceQuantities = {375, 625, 100, 10, 1, 2},
 	contribution = {100, 100, 100, 100, 100, 100},
 
 	targetTemplate = "object/tangible/deed/player_house_deed/naboo_house_large_deed.iff",
