41f0fc8810db5f30af40e3adafeccf35397f153e
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-08-17 14:32:07 -0500

    Update clothing_hat_chef_02.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_hat_chef_02.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_hat_chef_02.lua
@@ -65,10 +65,12 @@ object_draft_schematic_clothing_clothing_hat_chef_02 = object_draft_schematic_cl
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"shell", "binding_and_weatherproofing", "liner"},
    ingredientSlotType = {1, 0, 1},
-   resourceTypes = {"object/tangible/component/clothing/shared_fiberplast_panel.iff", "petrochem_inert", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
-   resourceQuantities = {3, 20, 1},
+   --resourceTypes = {"object/tangible/component/clothing/shared_fiberplast_panel.iff", "petrochem_inert", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "petrochem_inert", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   --resourceQuantities = {3, 20, 1},
+   resourceQuantities = {1, 20, 1},
    contribution = {100, 100, 100},
-
+--Starkiller - Equalized BE tissue across clothing.
 
    targetTemplate = "object/tangible/wearables/hat/hat_chef_s02.iff",
 
