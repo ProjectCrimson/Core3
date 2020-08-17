d6672378b979f276633a362af10f4bb71fd2f7ca
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-08-17 14:25:14 -0500

    Update clothing_hat_casual_10.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_hat_casual_10.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_hat_casual_10.lua
@@ -66,9 +66,10 @@ object_draft_schematic_clothing_clothing_hat_casual_10 = object_draft_schematic_
    ingredientTitleNames = {"shell", "binding_and_weatherproofing", "liner"},
    ingredientSlotType = {1, 0, 1},
    resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "petrochem_inert", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
-   resourceQuantities = {1, 25, 2},
+   --resourceQuantities = {1, 25, 2},
+   resourceQuantities = {1, 25, 1},
    contribution = {100, 100, 100},
-
+--Starkiller - Equalized BE tissue across clothing.
 
    targetTemplate = "object/tangible/wearables/hat/hat_s10.iff",
 
