3fefd8a9c854d4f82c1bf7e9254b641878943aae
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-08-21 15:01:30 -0500

    Update clothing_shirt_casual_42.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_shirt_casual_42.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_shirt_casual_42.lua
@@ -62,13 +62,19 @@ object_draft_schematic_clothing_clothing_shirt_casual_42 = object_draft_schemati
    customizationStringNames = {"/private/index_color_1", "/private/index_color_2"},
    customizationDefaults = {36, 24},
 
-   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"body", "binding_and_hardware"},
-   ingredientSlotType = {1, 0},
-   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast"},
-   resourceQuantities = {1, 30},
-   contribution = {100, 100},
-
+   --ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
+   --ingredientTitleNames = {"body", "binding_and_hardware"},
+   ingredientTitleNames = {"body", "lining", "binding_and_hardware"},
+   --ingredientSlotType = {1, 0},
+   ingredientSlotType = {1, 1, 0},
+   --resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast"},
+   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "fiberplast"},
+   --resourceQuantities = {1, 30},
+   resourceQuantities = {1, 1, 30},
+   --contribution = {100, 100},
+   contribution = {100, 100, 100},
+--Starkiller - Equalized BE tissue across clothing.
 
    targetTemplate = "object/tangible/wearables/shirt/shirt_s42.iff",
 
