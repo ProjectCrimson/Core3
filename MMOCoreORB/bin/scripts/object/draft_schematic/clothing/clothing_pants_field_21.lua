6e06ed0b6c12cf5883844da8cb241356e732718a
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-08-21 14:17:27 -0500

    Update clothing_pants_field_21.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_pants_field_21.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_pants_field_21.lua
@@ -65,10 +65,12 @@ object_draft_schematic_clothing_clothing_pants_field_21 = object_draft_schematic
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
    ingredientTitleNames = {"body", "binding_and_reinforcement", "hardware", "lining"},
    ingredientSlotType = {1, 0, 1, 1},
-   resourceTypes = {"object/tangible/component/clothing/shared_reinforced_fiber_panels.iff", "petrochem_inert", "object/tangible/component/clothing/shared_metal_fasteners.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
-   resourceQuantities = {1, 40, 2, 2},
+   --resourceTypes = {"object/tangible/component/clothing/shared_reinforced_fiber_panels.iff", "petrochem_inert", "object/tangible/component/clothing/shared_metal_fasteners.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceTypes = {"object/tangible/component/clothing/shared_synthetic_cloth.iff", "petrochem_inert", "object/tangible/component/clothing/shared_metal_fasteners.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   --resourceQuantities = {1, 40, 2, 2},
+   resourceQuantities = {1, 40, 2, 1},
    contribution = {100, 100, 100, 100},
-
+--Starkiller - Equalized BE tissue across clothing.
 
    targetTemplate = "object/tangible/wearables/pants/pants_s21.iff",
 
