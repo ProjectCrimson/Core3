d1a18b455097255734aeaa11810df3a4fbd2ace6
AWDEG <54282459+AWDEG@users.noreply.github.com>
2020-08-25 11:27:06 -0500

    Update clothing_jacket_formal_11.lua

--- MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_jacket_formal_11.lua
+++ MMOCoreORB/bin/scripts/object/draft_schematic/clothing/clothing_jacket_formal_11.lua
@@ -63,12 +63,16 @@ object_draft_schematic_clothing_clothing_jacket_formal_11 = object_draft_schemat
    customizationDefaults = {90},
 
    ingredientTemplateNames = {"craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n", "craft_clothing_ingredients_n"},
-   ingredientTitleNames = {"binding_and_hardware", "liner", "trim", "shell"},
-   ingredientSlotType = {0, 0, 1, 1},
-   resourceTypes = {"petrochem_inert_polymer", "hide_wooly", "object/tangible/component/clothing/shared_trim.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
-   resourceQuantities = {20, 25, 1, 1},
+   --ingredientTitleNames = {"binding_and_hardware", "liner", "trim", "shell"},
+   ingredientTitleNames = {"shell", "binding_and_hardware", "liner", "trim"},
+   --ingredientSlotType = {0, 0, 1, 1},
+   ingredientSlotType = {1, 0, 1, 1},
+   --resourceTypes = {"petrochem_inert_polymer", "hide_wooly", "object/tangible/component/clothing/shared_trim.iff", "object/tangible/component/clothing/shared_synthetic_cloth.iff"},
+   resourceTypes = {"object/tangible/component/clothing/shared_reinforced_fiber_panels.iff", "hide_wooly", "object/tangible/component/clothing/shared_synthetic_cloth.iff", "object/tangible/component/clothing/shared_trim.iff"},
+   --resourceQuantities = {20, 25, 1, 1},
+   resourceQuantities = {1, 45, 1, 1},
    contribution = {100, 100, 100, 100},
-
+--Starkiller - Equalized BE tissue across clothing.
 
    targetTemplate = "object/tangible/wearables/jacket/jacket_s11.iff",
 
